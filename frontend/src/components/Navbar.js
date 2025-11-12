2c705694cd68487f09a2798fd1566209c4b5a10b
mhancherick <141765650+mhancherick@users.noreply.github.com>
2025-11-12 18:03:05 -0500

    Added navbar and navbar CSS

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+        <div className="container">
+            <h2>De of Het?</h2>
+            <div>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </div>
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar;
