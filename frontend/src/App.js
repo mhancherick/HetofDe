15809968dd7d8450308f7f6b40a07db03f5bf3e5
mhancherick <141765650+mhancherick@users.noreply.github.com>
2025-11-13 11:14:27 -0500

    Updated routing for Grammar page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import About from './pages/About'
+import Grammar from './pages/Grammar';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/grammar" element={<Grammar />} />
       </Routes>
     </BrowserRouter>
   );
