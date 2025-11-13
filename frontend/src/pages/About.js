1f9fce71e358d632d42bcafe57694c7c740c4e48
mhancherick <141765650+mhancherick@users.noreply.github.com>
2025-11-13 11:34:22 -0500

    Updated About page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,7 +3,7 @@ function About() {
         <main className="container">
             <h1>About Het of De</h1>
             <p>One of the more difficult aspects of studying Dutch is determining the definite article
-                for nouns. There are rules, but there are so many exceptions to those rules that it's almost
+                for nouns. There are rules, but there are so many exceptions to those rules that it's 
                 better to ignore those rules entirely. Many of the online dictionaries that I found are in Dutch,
                 and the English based dictionaries did not allow me to easily determine what the article is.
             </p>
@@ -14,6 +14,8 @@ function About() {
 
             <p>Dutch data was sourced from <a href="https://kaikki.org">kaikki.org</a>. Many thanks to Tatu Ylonen for this resource!
             </p>
+
+            <p>If you're interested in diving into the code, here is a link to the <a href="https://github.com/mhancherick/HetofDe">Github repo!</a></p>
 
             <p>If you have suggestions, please email me at <a href="mailto:hancherm@oregonstate.edu">hancherm@oregonstate.edu</a></p>
         </main>
